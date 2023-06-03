ec2123b52148516f28d2b46a81e1e6002bcd31d9
Carl hope <84421131+carlhope@users.noreply.github.com>
2023-06-03 15:33:40 +0100

    Add project files.

---
+++ EntityFrameworkDB/DB.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDB
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string? StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public Grade? Grade { get; set; }
+    }
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string? GradeName { get; set; }
+        public string? Section { get; set; }
+
+        public ICollection<Student>? Students { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;Encrypt=false");  
+        }
+    }
+
+}
---
+++ EntityFrameworkDB/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EntityFrameworkDB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+                OutputWindow.Text = "it worked";
+            }
+        }
+    }
+}
